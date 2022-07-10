8991f3a1c60b6325d762f3aee33ff4b34bdfb4e6
Sara <sarahoubertz@gmail.com>
2022-07-10 20:33:53 +0200

    Finish 02_RepeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function repeat(input, times) {
+    let output = "";
+    if(times < 0)
+    {
+        return 'ERROR';
+    }
+    for(i = 0; i < times; i++)
+    {
+        output += input;
+    }
+    return output;
 };
 
 // Do not edit below this line
