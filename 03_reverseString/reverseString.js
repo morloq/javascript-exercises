5e6506dfe12878867cc3bba659ac26dd4c8d64b4
Sara <sarahoubertz@gmail.com>
2022-07-10 20:39:49 +0200

    Finish 03-ReverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+    for(i = input.length-1; i >=0; i--)
+    {
+        output += input.charAt(i);
+    }
+    return output;
 };
 
 // Do not edit below this line
