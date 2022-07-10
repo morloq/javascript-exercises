e64009e2c33a9e22bd953cde05db4ab86741bc79
Sara <sarahoubertz@gmail.com>
2022-07-10 23:36:53 +0200

    Completed? Not sure, test won't load

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ... theArgs) {
+    for(i = 0; theArgs.length; i++)
+    {
+        let index = arr.indexOf(theArgs[i]);
+        if(index != -1)
+        {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
