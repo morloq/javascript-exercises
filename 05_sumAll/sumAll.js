0a47bd99929d8c1d2ebba07d0c6db3935e443453
Sara <sarahoubertz@gmail.com>
2022-07-11 12:11:41 +0200

    Finish 06_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(start < 0)
+    {
+        return "ERROR";
+    }
+    if(end < 0)
+    {
+        return "ERROR";
+    }
+    if(isNaN(start) || typeof start ==='string')
+    {
+        return "ERROR";
+    }
+    if(isNaN(end) || typeof end === 'string')
+    {
+        return "ERROR";
+    }
+        if(start < end)
+        {
+            for(i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+        }
+        if(start > end)
+        {
+            for(i = end; i <= start; i++)
+            {
+                sum += i;
+            }
+        }
+    return sum;
 };
 
 // Do not edit below this line
