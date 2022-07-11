c6e2f6972883bcd622e3822192e4979d8b6f2759
Sara <sarahoubertz@gmail.com>
2022-07-11 12:29:27 +0200

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let output = (fahrenheit -32) * 5/9;
+  return Math.round(output * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let output = celsius * 9/5 +32;
+  return Math.round(output * 10) / 10;
 };
 
 // Do not edit below this line
