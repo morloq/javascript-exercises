d26e475055cdbceec8f0900a43b339d7309cf0d6
Sara <sarahoubertz@gmail.com>
2022-07-12 17:17:02 +0200

    Finish 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(input) {
+    //given array of objects that represents books -> title, author
+    //return an array containing all the titles of the given array of books
+    if(input.length == 0){
+        return "Empty";
+    }
 
+    let titles = [];
+
+    for(const key in input){
+        let obj = input[key];//get array
+        if(obj.hasOwnProperty("title")) {
+            titles.push(obj["title"]);
+        }
+    }
+    return titles;
 };
 
 // Do not edit below this line
