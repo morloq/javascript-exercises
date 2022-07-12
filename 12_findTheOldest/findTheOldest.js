43ac4079772a71e4bb1aff865fe408a13100d9a8
Sara <sarahoubertz@gmail.com>
2022-07-12 19:44:36 +0200

    Add comments + fix typo, works now

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -11,26 +11,23 @@ const findTheOldest = function(people) {
         }
     ];//supposed to store people by their ages
     let oldest = 0;
-    for(const key in people)
+    for(const key in people)//for each object in the array people
     {
-        let person = people[key];
+        let person = people[key];//get the object
         let age = 0;
-        if(person.hasOwnProperty("yearOfBirth") && person.hasOwnProperty("yearOfDeath")) {
-            age = person.yearOfDeath - person.yearOfBirth;
+        if(person.hasOwnProperty("yearOfBirth") && person.hasOwnProperty("yearOfDeath")) {//if person has date of birth and death
+            age = person.yearOfDeath - person.yearOfBirth;//calculate age based on that
         }
-        else{
-            age = new Date().getFullYear() - person.dateOfBirth;
+        else if(!person.hasOwnProperty("yearOfDeath")){//if person does not have a year of death
+            age = new Date().getFullYear() - person.yearOfBirth;//calculate age based on current year
         }
-        if(age > oldest && person.hasOwnProperty("yearOfDeath")) {
+        if(age > oldest) {
             oldest = age;
-            ageAndPeople[0] = person;
-        }
-        else if(age >= oldest){
-            oldest = age;
-            ageAndPeople[0] = person;
+            ageAndPeople[0] = person;///save person if age is higher than current oldest age
         }
     }
-    return ageAndPeople[0];
+    return ageAndPeople[0];//return oldest person
+    //return ray and not carly
 };
 
 // Do not edit below this line
