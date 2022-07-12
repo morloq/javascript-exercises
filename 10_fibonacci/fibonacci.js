3b4649298f51fea0fe25eedc3f47dc20fbe4d0ee
Sara <sarahoubertz@gmail.com>
2022-07-12 17:02:35 +0200

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    //do fibonacci sequence up until input
+    //return input
+    if(input < 0) {
+        return "OOPS";
+    }
+    else if(input == 0) {
+        return 0;
+    }
+    else if(input == 1){
+        return 1;
+    }
+    else{
+        let fibonacci;
+        //fibonnaci for input value:
+        fibonacci = Math.round(Math.pow((1/2)*(1+ Math.sqrt(5)), input)/(Math.sqrt(5)));
+        return fibonacci;
+    }
 };
 
 // Do not edit below this line
